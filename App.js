abf294b9b15b98f1209188c9c82330c259d365ce
신승훈 <>
2022-06-01 13:15:10 +0900

    global style 적용

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-web";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
 
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+};
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.testTest}>나의 첫번째 react native</Text>
-      <Button title="gg" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  testTest: {
-    marginBottom: 36,
+  appContaienr: {
+    padding: 100,
   },
 });
---
+++ screens/LoginScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native-web";
+
+const LoginScreen = () => {
+  return <View>gd</View>;
+};
+
+export default LoginScreen;
